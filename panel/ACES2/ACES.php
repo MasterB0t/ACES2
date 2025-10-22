8ea3a1a0eb7534c226c2eac4905fb85bb4fa57c7
masterb0t <chris.mntlv1@gmail.com>
2025-10-22 19:14:53 -0400

    Version Updated

--- panel/ACES2/ACES.php
+++ panel/ACES2/ACES.php
@@ -6,7 +6,7 @@ use Cache;
 
 class ACES {
 
-    const VERSION = '2.5.4';
+    const VERSION = '2.5.6';
     static function isNewVersion() {
 
         $Cache = new Cache('aces_version', 60 * 10 );
