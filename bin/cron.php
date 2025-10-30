cee06d9fc72d2ccaccfde4126e30a005a009657a
masterb0t <chris.mntlv1@gmail.com>
2025-10-30 12:23:59 -0400

    add automation event scan.

--- bin/cron.php
+++ bin/cron.php
@@ -11,7 +11,10 @@ switch(strtolower($argv[1])){
     case 'h':
         if( $SERVER_ID == 1 ) {
             exec("php /home/aces/bin/aces_auto_update_stream_name.php > /dev/null & ");
-            exec("php /home/aces/bin/iptv_automation_events.php > /dev/null & ");
+
+            $r=$db->query("SELECT id FROM iptv_dynamic_events WHERE active = 1 ");
+            while($event_id=$r->fetch_assoc()['id'])
+                exec("php /home/aces/bin/iptv_automation_events.php $event_id> /dev/null & ");
         }
 
         break;
